94b43698cd65593b96690009e513a1addc14509a
christianjunelm@gmail.com <christianjunelm@gmail.com>
2022-07-23 23:11:06 +0800

    fix: refactor random hex color generator

--- 2 - Intro To Web Dev/JS/Chapter 4/#1 Sticky Notes/index.js
+++ 2 - Intro To Web Dev/JS/Chapter 4/#1 Sticky Notes/index.js
@@ -9,9 +9,13 @@ window.onload = function onload() {
     }
 
     submitBtn.onclick = () => {
-        let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        var x=Math.round(0xffffff * Math.random()).toString(16);
+        var y=(6-x.length);
+        var z="000000";
+        var z1 = z.substring(0,y);
+        var color= "#" + z1 + x;
         notes.innerHTML += `
-        <div class="my-note" style="background-color: ${randomColor};">
+        <div class="my-note" style="background-color: ${color};">
             <article>
                 <p class="notes-info">${displayNote()}</p>
             </article>
