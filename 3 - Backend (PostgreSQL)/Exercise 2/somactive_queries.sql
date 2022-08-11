1290409b2da52f231615377e3fa8cbdaf91bb637
christianjunelm@gmail.com <christianjunelm@gmail.com>
2022-08-11 18:28:33 +0800

    fix: redo somactive_queries.sql

--- 3 - Backend (PostgreSQL)/Exercise 2/somactive_queries.sql
+++ 3 - Backend (PostgreSQL)/Exercise 2/somactive_queries.sql
@@ -4,15 +4,15 @@ CREATE DATABASE somactive;
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY NOT NULL,
-    username TEXT NOT NULL,
-    user_first_name TEXT NOT NULL,
-    user_last_name TEXT NOT NULL,
-    user_birthdate DATE NOT NULL,
-    user_profile_picture BYTEA NOT NULL,
-    user_email_address TEXT NOT NULL,
-    user_registration_date TIMESTAMP NOT NULL,
-    user_password TEXT NOT NULL,
-    user_sex VARCHAR(7) NOT NULL
+    username VARCHAR(60) NOT NULL,
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    birthdate DATE NOT NULL,
+    profile_picture VARCHAR(255) NOT NULL,
+    email_address VARCHAR(60) NOT NULL,
+    registration_date TIMESTAMP NOT NULL,
+    user_password VARCHAR(60) NOT NULL,
+    sex VARCHAR(7) NOT NULL
 );
 
 -- INSERT INTO users (username, user_first_name, user_last_name, user_birthdate, user_profile_picture, user_email_address, user_registration_date, user_password, user_sex)
@@ -39,7 +39,7 @@ CREATE TABLE details_for_macro_calculation (
 
 CREATE TABLE reminders_to_self (
     reminder_id SERIAL PRIMARY KEY NOT NULL,
-    user_reminder_to_self TEXT NOT NULL,
+    user_reminder_to_self VARCHAR(255) NOT NULL,
     inserted_at TIMESTAMP NOT NULL,
     user_id INT NOT NULL,
         FOREIGN KEY(user_id)
@@ -50,9 +50,6 @@ CREATE TABLE bmi_overview (
     bmi_id SERIAL PRIMARY KEY NOT NULL,
     weight_in_kg DEC(6,2) NOT NULL,
     height_in_cm DEC(6,2) NOT NULL,
-    bmi_score DEC(4,1) NOT NULL,
-    bmi_classification VARCHAR(15) NOT NULL,
-    bmi_description TEXT NOT NULL,
     inserted_at TIMESTAMP NOT NULL,
     user_id INT NOT NULL,
         FOREIGN KEY(user_id)
@@ -61,10 +58,10 @@ CREATE TABLE bmi_overview (
 
 CREATE TABLE workouts (
     workout_id SERIAL PRIMARY KEY NOT NULL,
-    workout_name TEXT NOT NULL,
-    workout_link TEXT NOT NULL,
-    workout_duration TIME NOT NULL,
-    workout_category VARCHAR(20) NOT NULL,
+    workout_name VARCHAR(255) NOT NULL,
+    link VARCHAR(100) NOT NULL,
+    duration TIME NOT NULL,
+    category VARCHAR(20) NOT NULL,
     bmi_id INT NOT NULL,
         FOREIGN KEY(bmi_id)
             REFERENCES bmi_overview(bmi_id)
